e395a88b946123c3e3d11e44504a3fe45e6843df
Jani Mikkonen <jani.mikkonen@gmail.com>
2022-05-20 13:43:29 +0300

    Initial code drop

---
+++ tasks.py
@@ -0,0 +1,40 @@
+# flake8: noqa
+from invoke import task, Collection
+from pathlib import Path
+
+
+assert Path.cwd() == Path(__file__).parent
+
+
+@task
+def reformat(ctx):
+    print("Reformatting code with black")
+    ctx.run("black src *.py")
+
+
+@task
+def black(ctx):
+    print("Verifying Black conformance")
+    ctx.run("black --check --diff --verbose src *.py")
+
+
+@task
+def flake8(ctx):
+    print("Running Flake8")
+    ctx.run("flake8 . --select=E9,F63,F7,F82 --show-source --statistics")
+    ctx.run("flake8 . --exit-zero --statistics")
+
+
+@task(post=[black, flake8])
+def check(ctx):
+    print("Running all checks")
+
+
+@task
+def example(ctx):
+    print("EXAMPLE: WIP")
+
+
+@task
+def test(ctx):
+    print("TEST: WIP")
