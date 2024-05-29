bf1f3ad1b91738350efdf46340f76185d8593e7a
Jani Mikkonen <jani.mikkonen@gmail.com>
2024-05-29 14:58:59 +0300

    Add support to generate extra report file (#16)
    Fixes #14

--- README.md
+++ README.md
@@ -10,22 +10,33 @@ to a PR - example output looks something like [this](https://github.com/rasjani/
 
 Install:
 
-```
+```shell
 python -mpip install robotframework-ghareports
 ```
 
 Usage:
 
-```
+```shell
 robot --listener GHAReports .
 ```
 
 If you want to limit the width of the tables in the summary, you can provide an argument to the listener like this:
 
 
-```
+```shell
 robot --listener GHAReports:35 .
 ```
 
 This will split each test case name at every 35 characters into a new line.
 
+And if you want to generate the summary even  if a) you are not running in Github *or* you want to generate extra summary file independent of
+Github actions, pass report_file argument to the listener like this:
+
+```shell
+robot --listener GHAReports:report_file=extra_summary.md
+```
+or
+
+```shell
+robot --listener GHAReports:34:extra_summary.md
+```
--- utest/tests.py
+++ utest/tests.py
@@ -2,28 +2,59 @@ import sys
 import os
 import contextlib
 from pathlib import Path
+
 sys.path.append(Path(__file__).parent / ".." / "src")
 from GHAReports import GHAReports
 
 
 @contextlib.contextmanager
-def set_env(**environ):
-    old_environ = dict(os.environ)
-    os.environ.update(environ)
+def modified_environ(*remove, **update):
+    env = os.environ
+    update = update or {}
+    remove = remove or []
+
+    stomped = (set(update.keys()) | set(remove)) & set(env.keys())
+    update_after = {k: env[k] for k in stomped}
+    remove_after = frozenset(k for k in update if k not in env)
+
     try:
+        env.update(update)
+        [env.pop(k, None) for k in remove]
         yield
     finally:
-        os.environ.clear()
-        os.environ.update(old_environ)
+        env.update(update_after)
+        [env.pop(k) for k in remove_after]
+
 
 def test_no_github():
-  with set_env(GITHUB_STEP_SUMMARY=""):
-    r = GHAReports()
-    assert r.initialized == False
-    #assert r._report_file == None
+    with modified_environ("GITHUB_STEP_SUMMARY"):
+        r = GHAReports()
+        assert r.initialized == False
+        assert r._report == None
+        assert r._output == None
+
 
 def test_with_github():
-  with set_env(GITHUB_STEP_SUMMARY="/foo/bar"):
-    r = GHAReports()
-    assert r.initialized == True
-    assert r._output == Path("/foo/bar")
+    with modified_environ(GITHUB_STEP_SUMMARY="/foo/bar"):
+        r = GHAReports()
+        assert r.initialized == True
+        assert r._output == Path("/foo/bar")
+        assert r._report == None
+
+
+def test_extra_report_file():
+    with modified_environ(GITHUB_STEP_SUMMARY="/foo/bar"):
+        r = GHAReports(report_file="/bar/foo")
+        assert r.initialized == True
+        assert r._output == Path("/foo/bar")
+        assert r._report == Path("/bar/foo")
+
+
+def test_writing(tmp_path):
+    step_summary = tmp_path / "step_summary.md"
+    extra_report = tmp_path / "extra.md"
+    with modified_environ(GITHUB_STEP_SUMMARY=f"{step_summary}"):
+        r = GHAReports(report_file=f"{extra_report}")
+        r.close()
+        assert step_summary.exists() == True
+        assert extra_report.exists() == True
